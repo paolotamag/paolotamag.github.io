5d41aba8f81ccf6f89c81f699d8f3a7e6417a562
Paolo Tamagnini <paolotamag@gmail.com>
2024-06-24 16:09:46 +0200

    try new behaviour for expanding sections

--- js/my.js
+++ js/my.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
 
 	$('.majorpointslegend').click(function(){
 	    $(this).next().toggle();
+		// Check if the text is '>' and change it to '*'
+		if ($(this).text() === ' &#9658; ... ') {
+			$(this).text(' &#9660; ');
+		} else if ($(this).text() === ' &#9660;' ) { // Optional: Change it back to '>' if you click again
+			$(this).text(' &#9658; ... ');
+		}
 	});
 
 });
